540f099de642f0f6911cd3109e475131602df1c8
tanbinakter <tanbinakter.cse@gmail.com>
2026-02-23 15:41:19 +0600

    this is react class 1

---
+++ React_course/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <button className="bg-red-700 text-white px-5 py-2 rounded-lg">
+        Click Me
+      </button>
+    </div>
+  );
+};
+
+export default Home;
